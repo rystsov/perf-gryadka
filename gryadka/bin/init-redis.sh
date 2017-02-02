aea334b695d084cde521ae21ce77af0a18f6f8a4
Denis Rystsov <rystsov.denis@gmail.com>
2017-02-02 08:50:38 -0800

    updates script to use right redis's host

--- gryadka/bin/init-redis.sh
+++ gryadka/bin/init-redis.sh
@@ -6,6 +6,7 @@ mkdir -p deployment/$1
 node src/redis-conf.js $1 $(pwd)/deployment/$1
 
 port=$(cat etc/settings.json | jq ".acceptors.$1.storage.port")
+host=$(cat etc/settings.json | jq ".acceptors.$1.storage.host")
 echo $port
 echo $(cat node_modules/gryadka/src/lua/accept.lua)
 
@@ -13,6 +14,6 @@ redis-server deployment/$1/redis.conf &
 PID=$!
 # I don't have an excuse for sleep
 sleep 1
-redis-cli -p $port SCRIPT LOAD "$(cat node_modules/gryadka/src/lua/accept.lua)" > deployment/$1/accept.hash
-redis-cli -p $port SCRIPT LOAD "$(cat node_modules/gryadka/src/lua/prepare.lua)" > deployment/$1/prepare.hash
+redis-cli -h $host -p $port SCRIPT LOAD "$(cat node_modules/gryadka/src/lua/accept.lua)" > deployment/$1/accept.hash
+redis-cli -h $host -p $port SCRIPT LOAD "$(cat node_modules/gryadka/src/lua/prepare.lua)" > deployment/$1/prepare.hash
 kill $PID
